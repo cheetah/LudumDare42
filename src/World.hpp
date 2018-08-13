f99792a39a41e50cd60808f4aafdcb89236ac110
Anton Zadvorny <thcheetah@gmail.com>
2018-08-14 00:35:42 +0300

    Add more events

--- src/World.hpp
+++ src/World.hpp
@@ -104,6 +104,10 @@ namespace Event {
     Flare,
     Radio,
     SpoiledFood,
+    StarStorm,
+    GasFood,
+    Newcomers,
+    Landfall,
   };
 
   struct Step {
@@ -471,6 +475,214 @@ private:
     }
   };
 
+  Event::Info starStormEvent = Event::Info{
+    .type = Event::Type::StarStorm,
+    .steps = std::map<int, Event::Step>{
+      {
+        0,
+        Event::Step{
+          .text = "Star storm was detected by the equipment nearby. We can collect many useful data inside.",
+          .diff = std::map<Resource, int>{},
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {1, "Send a research group of 5 people"},
+            {2, "Send a research group of 15 people"},
+            {3, "Try to remotely scan the anomaly"},
+            {4, "Ignore"}
+          }
+        }
+      }, {
+        1,
+        Event::Step{
+          .text = "You receives a message from the expedition. They says that research still not finished, but situation inside becomes dangerous very fast.",
+          .diff = std::map<Resource, int>{},
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {5, "Order them return immediately"},
+            {6, "Order them to finish research"},
+          }
+        }
+      }, {
+        2,
+        Event::Step{
+          .text = "You receives a message from the expedition. They says that research still not finished, but situation inside becomes dangerous very fast.",
+          .diff = std::map<Resource, int>{},
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {5, "Order them return immediately"},
+            {7, "Order them to finish research"},
+          }
+        }
+      }, {
+        3,
+        Event::Step{
+          .text = "You remotely scan the storm. This brings a bit of scientific data, but no so much as if you research it from inside. (+30 science)",
+          .diff = std::map<Resource, int>{ {Resource::Science, 30} },
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }, {
+        4,
+        Event::Step{
+          .text = "You decided to focus on survival.",
+          .diff = std::map<Resource, int>{},
+          .ignoreCheck = true,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }, {
+        5,
+        Event::Step{
+          .text = "Research group returns with a bit of data. Who knows how many unexplored things has left inside the anomaly. (+40 science)",
+          .diff = std::map<Resource, int>{ {Resource::Science, 40} },
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }, {
+        6,
+        Event::Step{
+          .text = "You receiving scientific data for an about hour. After that the transfer is interrupts. You wait for the research team until the end of the day, but no one comes back. (+120 science -5 people)",
+          .diff = std::map<Resource, int>{ {Resource::Science, 120}, {Resource::Peoples, -5} },
+          .ignoreCheck = true,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }, {
+        7,
+        Event::Step{
+          .text = "You receiving scientific data for an about hour. After that the transfer is interrupts. You wait for the research team until the end of the day, but no one comes back. (+300 science -15 people)",
+          .diff = std::map<Resource, int>{ {Resource::Science, 300}, {Resource::Peoples, -15} },
+          .ignoreCheck = true,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }
+    }
+  };
+
+  Event::Info gasFoodEvent = Event::Info{
+    .type = Event::Type::GasFood,
+    .steps = std::map<int, Event::Step>{
+      {
+        0,
+        Event::Step{
+          .text = "Scientists reports that theoretically there is an opportunity to convert food into gas.",
+          .diff = std::map<Resource, int>{},
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue without experiments"},
+            {1, "Convert 20 food to gas"},
+            {2, "Convert 50 food to gas"},
+            {3, "Convert 150 food to gas"},
+          }
+        }
+      }, {
+        1,
+        Event::Step{
+          .text = "The experiment was successful. (-20 food +20 gas)",
+          .diff = std::map<Resource, int>{ {Resource::Food, -20}, {Resource::Gas, 20} },
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }, {
+        2,
+        Event::Step{
+          .text = "The experiment was successful. (-50 food +50 gas)",
+          .diff = std::map<Resource, int>{ {Resource::Food, -50}, {Resource::Gas, 50} },
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }, {
+        3,
+        Event::Step{
+          .text = "The experiment was successful. (-150 food +150 gas)",
+          .diff = std::map<Resource, int>{ {Resource::Food, -150}, {Resource::Gas, 150} },
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }
+    }
+  };
+
+  Event::Info newcomersEvent = Event::Info{
+    .type = Event::Type::Newcomers,
+    .steps = std::map<int, Event::Step>{
+      {
+        0,
+        Event::Step{
+          .text = "A rescue pod landed nearby. It looks like another research team members were able to survive the crash. They're joining the camp. (+12 people)",
+          .diff = std::map<Resource, int>{ {Resource::Peoples, 12} },
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"}
+          }
+        }
+      }
+    }
+  };
+
+  Event::Info landfallEvent = Event::Info{
+    .type = Event::Type::Landfall,
+    .steps = std::map<int, Event::Step>{
+      {
+        0,
+        Event::Step{
+          .text = "Some tiles are about to collapse.",
+          .diff = std::map<Resource, int>{},
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {1, "Use minerals to stop the destruction (-10 minerals)"},
+            {2, "Use science to stop the destruction (-3 science)"},
+            {3, "Ignore"},
+          }
+        }
+      }, {
+        1,
+        Event::Step{
+          .text = "You prevented destruction with minerals.",
+          .diff = std::map<Resource, int>{ {Resource::Minerals, -10} },
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"},
+          }
+        }
+      }, {
+        2,
+        Event::Step{
+          .text = "You prevented destruction with scientific data.",
+          .diff = std::map<Resource, int>{ {Resource::Science, -3} },
+          .ignoreCheck = false,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"},
+          }
+        }
+      }, {
+        3,
+        Event::Step{
+          .text = "You did nothing and unfortunately, 2 tiles has fallen off.",
+          .diff = std::map<Resource, int>{ {Resource::Tiles, -2} },
+          .ignoreCheck = true,
+          .choices = std::map<int, std::string>{
+            {-1, "Continue"},
+          }
+        }
+      }
+    }
+  };
+
   std::map<Event::Type, Event::Info*> eventInfos = {
     {Event::Type::Start, &startEvent},
     {Event::Type::Win, &winEvent},
@@ -480,6 +692,10 @@ private:
     {Event::Type::Flare, &flareEvent},
     {Event::Type::Radio, &radioEvent},
     {Event::Type::SpoiledFood, &spoiledFoodEvent},
+    {Event::Type::StarStorm, &starStormEvent},
+    {Event::Type::GasFood, &gasFoodEvent},
+    {Event::Type::Newcomers, &newcomersEvent},
+    {Event::Type::Landfall, &landfallEvent},
   };
 
   std::vector<Event::Type> randomEvents = {
@@ -488,6 +704,10 @@ private:
     Event::Type::Flare,
     Event::Type::Radio,
     Event::Type::SpoiledFood,
+    Event::Type::StarStorm,
+    Event::Type::GasFood,
+    Event::Type::Newcomers,
+    Event::Type::Landfall,
   };
 public:
   World();
